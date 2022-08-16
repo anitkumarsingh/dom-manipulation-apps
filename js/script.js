8e6e9143c15612784df192113222532f0ac669c9
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-16 22:34:54 +0530

    dom selection

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const selectBtn = document.querySelectorAll('.btn');
+const selectModal = document.querySelector('.modal');
+const selectCloseBtn = document.querySelector('.btn-close');
+const selectOverlay = document.querySelector('.modal-overlay');
+
+
+for(let i = 0; i<selectBtn.length;i++){
+  console.log(selectBtn[i].textContent);
+}
