88d50f89ac48b3d4d2245ff123bbb10fc6cbb613
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-16 23:18:28 +0530

    modal functionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,17 @@ const selectModal = document.querySelector('.modal');
 const selectCloseBtn = document.querySelector('.btn-close');
 const selectOverlay = document.querySelector('.modal-overlay');
 
-
-for(let i = 0; i<selectBtn.length;i++){
-  console.log(selectBtn[i].textContent);
+for (let i = 0; i < selectBtn.length; i++) {
+  selectBtn[i].addEventListener('click', function () {
+    console.log(selectBtn[i].textContent);
+    selectModal.classList.remove('hidden');
+    selectOverlay.classList.remove('hidden');
+  });
 }
+
+const closeModal  = () =>{
+  selectModal.classList.add('hidden');
+  selectOverlay.classList.add('hidden');
+}
+selectCloseBtn.addEventListener('click', closeModal);
+selectOverlay.addEventListener('click',closeModal);
