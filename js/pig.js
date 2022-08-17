b284f5a482898c125570f274d0c56d5fc2d809cc
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-17 17:13:29 +0530

    current player score

--- js/pig.js
+++ js/pig.js
@@ -2,6 +2,8 @@
 // Selecting elements
 const playerZeroScore = document.querySelector('#score--0');
 const playerOneScore = document.querySelector('#score--1');
+const currentPlayer1Score = document.getElementById('current--0');
+const currentPlayer2Score = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
 const rollDiceEle = document.querySelector('.btn--roll');
 
@@ -10,29 +12,17 @@ playerZeroScore.textContent = 0;
 playerOneScore.textContent = 0;
 dice.classList.add('hidden');
 
+let currentScore = 0;
 // Attaching event on roll-dice btn
 rollDiceEle.addEventListener('click', function () {
   const diceRandomNo = Math.trunc(Math.random() * 6) + 1;
   dice.classList.remove('hidden');
+  dice.src = `../img/dice-${diceRandomNo}.png`;
 
-  switch (diceRandomNo) {
-    case 1:
-      dice.src = `../img/dice-1.png`;
-      break;
-    case 2:
-      dice.src = `../img/dice-2.png`;
-      break;
-    case 3:
-      dice.src = `../img/dice-3.png`;
-      break;
-    case 4:
-      dice.src = `../img/dice-4.png`;
-      break;
-    case 5:
-      dice.src = `../img/dice-5.png`;
-      break;
-    case 6:
-      dice.src = `../img/dice-6.png`;
-      break;
+  if (diceRandomNo !== 1) {
+    currentScore += diceRandomNo;
+    currentPlayer1Score.textContent = currentScore;
+  } else {
+    // switch player
   }
 });
