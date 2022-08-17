40854bbc0b04569d2a15221bdf2c4b57cfb0afb1
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-17 17:58:02 +0530

    switch between players

--- js/pig.js
+++ js/pig.js
@@ -4,8 +4,13 @@ const playerZeroScore = document.querySelector('#score--0');
 const playerOneScore = document.querySelector('#score--1');
 const currentPlayer1Score = document.getElementById('current--0');
 const currentPlayer2Score = document.getElementById('current--1');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
 const dice = document.querySelector('.dice');
 const rollDiceEle = document.querySelector('.btn--roll');
+const newGame = document.querySelector('.btn--new');
+const holdGame = document.querySelector('.btn--hold');
 
 // Starting conditions
 playerZeroScore.textContent = 0;
@@ -13,6 +18,7 @@ playerOneScore.textContent = 0;
 dice.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 // Attaching event on roll-dice btn
 rollDiceEle.addEventListener('click', function () {
   const diceRandomNo = Math.trunc(Math.random() * 6) + 1;
@@ -21,8 +27,19 @@ rollDiceEle.addEventListener('click', function () {
 
   if (diceRandomNo !== 1) {
     currentScore += diceRandomNo;
-    currentPlayer1Score.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    if(activePlayer === 0){
+      player1.classList.remove('player--active');
+      player2.classList.add('player--active');
+    }else{
+      player2.classList.remove('player--active');
+      player1.classList.add('player--active');
+    }
   }
 });
